49ff4a1671b4d9a9fa19396177a323f21ddff331
Faruq2014 <mjmolla@gmail.com>
2020-08-28 18:26:05 -0400

    First commit

---
+++ SalesForcePageFactory/src/test/java/com/SalesForcePageFactory/login/LoginTest.java
@@ -0,0 +1,10 @@
+package com.SalesForcePageFactory.login;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+@Test
+public void loginTestWithValidCredential() {
+	System.out.println("Assalamualaikum");
+}
+}
