a21f208054696c2a3f23a711c1f63b1484148af0
Faruq2014 <mjmolla@gmail.com>
2020-08-30 00:05:56 -0400

    git 2nd commit

--- SalesForcePageFactory/src/main/java/com/SalesForcePageFactory/loginPage/LoginPage.java
+++ SalesForcePageFactory/src/main/java/com/SalesForcePageFactory/loginPage/LoginPage.java
@@ -1,10 +1,58 @@
 package com.SalesForcePageFactory.loginPage;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
 public class LoginPage {
 
-	public static void main(String[] args) {
-		System.out.println("Assalamualaikum");
+	
+	  public WebDriver driver;
+	  
+	  public LoginPage(WebDriver driver) { this.driver= driver; } 
+	  //user define constructor
+	  // i really do not need this constructor, just to show how it works with 
+	  // zero arguments constructor or super constructor below. 
+	  //it is recommended to create zero arguments constructor with user define  constructor.
+	  public LoginPage() { super();
+	  
+	  }
+	 
+	@FindBy(xpath="//img[@class='img-responsive']")
+		public WebElement login_Link;
+		
+
+		public void clickOnLoginLink() {
+			login_Link.click();
+			
+		}
+		
+		//driver.findElement(By.xpath("(//div[@class='dropdown']/a)[2]")).click();
+		
+		@FindBy(how = How.ID, using = "username")private WebElement userid;
+		
+		public void enterYourUserName(String enterUserId) {
+			userid.sendKeys(enterUserId);
+			
+		}
+		
+		@FindBy(how = How.CSS, using = "#password")private WebElement password;
+		
+		public void enterYourPassword(String enterPassword) {
+			password.sendKeys(enterPassword);
+			
+		}
+		
+		
+		@FindBy(how = How.CSS, using = "#Login")private WebElement submit;
+		
+		public void clickOnSubmitButton() {
+			submit.sendKeys(Keys.ENTER);
+			
+		}
 
 	}
 
-}
+
--- SalesForcePageFactory/src/test/java/com/SalesForcePageFactory/login/LoginTest.java
+++ SalesForcePageFactory/src/test/java/com/SalesForcePageFactory/login/LoginTest.java
@@ -1,10 +1,50 @@
 package com.SalesForcePageFactory.login;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.SalesForcePageFactory.loginPage.LoginPage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LoginTest {
+	public  WebDriver driver;
+	
+	@BeforeClass
+	public void setUP() {
+		//System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDriver\\chromedriver_win32\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup(); 
+		ChromeOptions options = new ChromeOptions();
+		System.setProperty("webdriver.chrome.silentOutput","true"); 
+		driver = new ChromeDriver(options);
+		driver.get("https://www.salesforce.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
 @Test
 public void loginTestWithValidCredential() {
 	System.out.println("Assalamualaikum");
+	// both constructors are availabe, we are using super constructor.
+	//LoginPage lp = new LoginPage(driver); 
+	LoginPage lp = new LoginPage();
+	PageFactory.initElements(driver, lp);
+	lp.clickOnLoginLink();
+	lp.enterYourUserName("aaaaaaa");
+	lp.enterYourPassword("1234");
+	lp.clickOnSubmitButton();
 }
+
+@AfterClass
+public void destroy_setUp() {
+	driver.quit();
+}
+
 }
