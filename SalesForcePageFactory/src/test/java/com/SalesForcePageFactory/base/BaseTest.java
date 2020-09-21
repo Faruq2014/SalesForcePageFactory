ef50d485c675a6a51e4a045112c35408b88404e7
Faruq2014 <mjmolla@gmail.com>
2020-09-21 13:07:15 -0400

    log4j2

--- SalesForcePageFactory/src/test/java/com/SalesForcePageFactory/base/BaseTest.java
+++ SalesForcePageFactory/src/test/java/com/SalesForcePageFactory/base/BaseTest.java
@@ -1,65 +1,72 @@
-package com.SalesForcePageFactory.base;
+	package com.SalesForcePageFactory.base;
+	
+	import java.io.FileInputStream;
+	import java.io.FileNotFoundException;
+	import java.io.IOException;
+	import java.util.Properties;
+	import java.util.concurrent.TimeUnit;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.concurrent.TimeUnit;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class BaseTest {
-public static WebDriver driver;
-
-public static Properties config= new Properties();
-public static FileInputStream fis;
-public static String projectPath=System.getProperty("user.dir");
-public static String browser;//may be we do not need
-
-@BeforeSuite
-public void setUp() {
-	if (driver==null) {
-		try {
-			fis=new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\properties\\Config.properties");
-		} catch (FileNotFoundException e) {
-		
-			e.printStackTrace();
-		}
-
-		try {
-			config.load(fis);
-		} catch (IOException e) {
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.openqa.selenium.firefox.FirefoxDriver;
+	import org.testng.annotations.AfterSuite;
+	import org.testng.annotations.BeforeSuite;
 	
-			e.printStackTrace();
-		}
-	}
+	import io.github.bonigarcia.wdm.WebDriverManager;
 	
-	if (config.getProperty("browser").equalsIgnoreCase("chrome")) {
-		WebDriverManager.chromedriver().setup(); 
-		driver = new ChromeDriver();
+	public class BaseTest {
+	public static WebDriver driver;
+	
+	public static Properties config= new Properties();
+	public static FileInputStream fis;
+	public static String projectPath=System.getProperty("user.dir");
+	public static String browser;//may be we do not need
+	public static Logger log=LogManager.getLogger(BaseTest.class);
+	@BeforeSuite
+	public void setUp() {
+		if (driver==null) {
+			try {
+				fis=new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\properties\\Config.properties");
+			log.info("properties path loaded. add something");
+			} catch (FileNotFoundException e) {
 			
-	}else if (config.getProperty("browser").equalsIgnoreCase("firefox")) {
-		WebDriverManager.firefoxdriver().setup();
-		System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "null");
-		driver = new FirefoxDriver();
+				e.printStackTrace();
+			}
+	
+			try {
+				config.load(fis);
+			} catch (IOException e) {
+		
+				e.printStackTrace();
+			}
+		}
+		
+		if (config.getProperty("browser").equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup(); 
+			driver = new ChromeDriver();
+			log.info("chrome browser loaded add something");
+				
+		}else if (config.getProperty("browser").equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "null");
+			driver = new FirefoxDriver();
+			log.info("chrome browser loaded add something");
+		}
+		
+		driver.get(config.getProperty("url"));
+		log.warn("Salesforce open");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
+		
+	}
+	 @AfterSuite
+	public void tearDown() {
+		if (driver!=null) {
+		driver.quit();
+		log.info("driver close add something");
+		}
 	}
 	
-	driver.get(config.getProperty("url"));
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
-	
-}
- @AfterSuite
-public void tearDown() {
-	if (driver!=null) {
-	driver.quit();	
 	}
-}
-
-}
--- SalesForcePageFactory/src/test/java/com/SalesForcePageFactory/login/Login_WithConstructorTest.java
+++ SalesForcePageFactory/src/test/java/com/SalesForcePageFactory/login/Login_WithConstructorTest.java
@@ -1,37 +1,24 @@
 package com.SalesForcePageFactory.login;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.SalesForcePageFactory.base.BaseTest;
 import com.SalesForcePageFactory.loginPage.Login_WithConstructor;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class Login_WithConstructorTest {
-	public static WebDriver driver;
-	@BeforeClass
-	public void setUP() {
-		//System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDriver\\chromedriver_win32\\chromedriver.exe");
-		WebDriverManager.chromedriver().setup(); 
-		driver = new ChromeDriver();
-		driver.get("https://www.salesforce.com/");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
+public class Login_WithConstructorTest extends BaseTest{
+	
+	
 	
 	@Test(priority=1)
+
 	public void loginWithValidCredentialTest() {
+		log.info("login test started");
 		Login_WithConstructor login = new Login_WithConstructor(driver);
 		login.loginPage("dddd", "1234");
-		
+		log.info("user name and password inserted");
+		log.info("log in test finish");
+	
 	}
 	
 	
-	  @AfterClass public void destroy_setUp() { driver.quit(); }
-	 
 }
